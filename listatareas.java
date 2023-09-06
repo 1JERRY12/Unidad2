bfb89dd85a7a2037e50a9da90b75443d40af5896
1JERRY12 <143622260+1JERRY12@users.noreply.github.com>
2023-09-06 08:53:40 -0600

    Add files via upload

---
+++ listatareas.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class listatareas {
+    
+    public static void main(String[] args) {
+        String tarea1 = "Lavar los trastes";
+        String tarea2 = "Hacer la tarea";
+        String tarea3 = "Limpiar la casa";
+        String tarea4 = "Trabajar";
+        String tarea5 = "Podar el pasto";
+        
+        Sout("mi tarea(es"podar el pásto");
+        
+        
+    }
+  
+            
+    
+    
+}
